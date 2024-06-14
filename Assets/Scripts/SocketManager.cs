22aebdf37f63f45062ec0bb89b2ccd82e361bd20
soemyatmin <myatmin.soe@tecotec.co.jp>
2024-06-15 02:50:22 +0900

    add SimpleJSON package

--- Assets/Scripts/SocketManager.cs
+++ Assets/Scripts/SocketManager.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System;
 using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
+using SimpleJSON;
 
 public class SocketManager : Singleton<SocketManager> {
 	private SocketIOUnity client;
@@ -33,6 +34,7 @@ public class SocketManager : Singleton<SocketManager> {
 				// Assuming response is a JSON string
 				string json = response.ToString();
 				Debug.Log($"{json}");
+				ReadAsData(json);
 
 				// Deserialize JSON string to dictionary
 				Dictionary<string, PlayerData> currentPlayers = JsonConvert.DeserializeObject<Dictionary<string, PlayerData>>(json);
@@ -82,4 +84,37 @@ public class SocketManager : Singleton<SocketManager> {
 			position = new { x = position.x, y = position.y }
 		});
 	}
+
+	void ReadAsData(string jsonText)
+    {
+        JSONNode Node = JSONNode.Parse(jsonText);
+
+        int maxNum = Node.Count;
+        string playerDataid = "";
+        string playerPositionX = "";
+        string playerPositionY = "";
+        foreach (KeyValuePair<string, JSONNode> kvp in (JSONObject)Node[0])
+        {
+            Debug.Log(string.Format("SMM : {0}: {1} {2}",
+                kvp.Key, kvp.Value["position"]["x"].Value, kvp.Value["position"]["y"].Value));
+
+            Debug.Log("SMM kvp.Key " + kvp.Key.ToString());
+            Debug.Log("SMM playerData.position.x " + kvp.Value["position"]["x"].Value.ToString());
+            Debug.Log("SMM playerData.position.y " + kvp.Value["position"]["x"].Value.ToString());
+
+            playerDataid = kvp.Key.ToString();
+            playerPositionX = kvp.Value["position"]["x"].Value.ToString();
+            playerPositionY = kvp.Value["position"]["x"].Value.ToString();
+        }
+        Debug.Log("SMM almost Printing");
+
+        Debug.Log("SMM kvp.Key " + playerDataid);
+        Debug.Log("playerPositionX " + playerPositionX);
+        Debug.Log("playerPositionY " + playerPositionY);
+        
+        SMMPlayerData SMMplayerData = new SMMPlayerData(playerDataid, int.Parse(playerPositionX), int.Parse(playerPositionY));
+        SMMplayerData.Print();
+        
+
+    }
 }
